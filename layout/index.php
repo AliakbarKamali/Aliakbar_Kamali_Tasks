089d37b6c8c4fe1b2df0806e62755b3e9b05d4ab
AliakbarKamali <aliakbar.kamaliandani@student.hamk.fi>
2023-02-01 09:28:15 +0200

    layout

---
+++ layout/index.php
@@ -0,0 +1,12 @@
+<?php include "header.php";?>
+
+<h1>Some content</h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+     Repellat et, dolores dignissimos ab reprehenderit accusantium voluptatum neque nemo magnam enim, 
+     fuga ratione? Consequatur facere repellat animi error dignissimos quasi quae.
+</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+     Repellat et, dolores dignissimos ab reprehenderit accusantium voluptatum neque nemo magnam enim, 
+     fuga ratione? Consequatur facere repellat animi error dignissimos quasi quae.
+</p>
+<?php include "footer.php";?>
