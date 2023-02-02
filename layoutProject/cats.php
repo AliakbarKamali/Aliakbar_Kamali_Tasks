7d8d6b5552d07023ffd0cebb83be7e0ae1577d1b
AliakbarKamali <aliakbar.kamaliandani@student.hamk.fi>
2023-02-02 22:18:42 +0200

    edit pages of project

--- layoutProject/cats.php
+++ layoutProject/cats.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-    <title>Catffee</title>
-    <link rel="stylesheet" href="cats.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;1,100;1,600&family=Roboto:wght@100;300;500&display=swap" rel="stylesheet">
-
-</head>
-<body>
-    <div class="container">
-    
-   <nav>
-    <div class = "logoText">
-    <div class = "navItem1"><img src="./images/logo.png" alt="Logo"></div>
-     <div class = "navItem2">Catffee</div>
-    </div>
-    <div class = "navRest">
-        <div class = "navItem3"><a href="index.html">About us </a></div>
-        <div class = "navItem4"><a href="menu.html">Menu</div></a>
-        <div class = "navItem5"><a href="cats.html">Cats</div></a>
-        <div class = "navItem6"><a href="contact.html">Contact</div></a>
-    </div>
-   </nav>
+<?php $style = "cats_style"; $title = "Exercise1: Hamk oy"; include "header.php";?>
    <section>
     <div class = "coverImage">
        <img class="img-fluid" src="images/background7.jpeg" alt="coverImage">
@@ -90,14 +64,4 @@
     </div>
 
    </section>
-   <footer>
-    <div class = "footer">
-        <div class = "footerInsta"><i class="fa-brands fa-instagram"></i></div>
-        <div class = "footerFB"><i class="fa-brands fa-facebook"></i></div>
-        <div class = "footerTwitter"><i class="fa-brands fa-twitter"></i></div>
-
-    </div>
-   </footer>
-</div>
-</body>
-</html>
+   <?php include "footer.php";?>
--- layoutProject/header.php
+++ layoutProject/header.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 
  
-    <link rel="stylesheet" href="heder_style.css">
+    <link rel="stylesheet" href="footer_style.css">
     <link rel="stylesheet" href="<?php echo $style ?>.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;1,100;1,600&family=Roboto:wght@100;300;500&display=swap" rel="stylesheet">
   
@@ -22,7 +22,7 @@
      <div class = "navItem2">Catffee</div>
     </div>
     <div class = "navRest">
-      <div class = "navItem3"><a href="index.php">About us</a></div>
+      <div class = "navItem3"><a href="aboutus.php">About us</a></div>
       <div class = "navItem4"><a href="menu.php">Menu</a></div>
       <div class = "navItem5"><a href="cats.php">Cats</a></div>
       <div class = "navItem6"><a href="contact.php">Contact</a></div>
