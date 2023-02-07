2a80d8f60c0ba5e824aef7fc819c7851464545ef
AliakbarKamali <aliakbar.kamaliandani@student.hamk.fi>
2023-02-07 10:29:23 +0200

    placeholder

--- layout/variable.php
+++ layout/variable.php
@@ -5,11 +5,24 @@
     1.Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using <h3> tag: Hello …., You are welcome to my site.  (Form Tutorial: https://www.youtube.com/watch?v=nLPlDHrsyYA  
 </h2>
 <form action="action.php" method="post">
-  First Name:  <input type="text" name="fname" required> <br>
-  Last Name:  <input type="text" name="lname" required> <br>
+  <div class="row">
+
+    <div class="col">
+    <input type="text" name="fname" required placeholder="First Name" class="form-control">
+    </div>
+
+    <div class="col">
+    <input type="text" name="lname" required placeholder="Last Name" class="form-control">
+    </div>
+  </div>
+
   Birth Date:  <input type="date" name="bdate"> <br>
   Select fav Color:  <input type="color" name="color"> <br>
   <input type="submit" name="Submit">
+
+
+
+
 
 </form>
 
