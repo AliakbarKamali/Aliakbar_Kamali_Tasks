897e7b61370a07fd76a93dbb39e59448e19ad241
AliakbarKamali <aliakbar.kamaliandani@student.hamk.fi>
2023-02-07 10:09:39 +0200

    change folder

---
+++ layout/action.php
@@ -0,0 +1,21 @@
+<?php include "header.php";
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$dob = $_POST['bdate'];
+$color = $_POST['color'];
+
+echo "<h3> Yore name is $fname $lname  and your Date of Birth is $dob and your favorite color is $color .
+
+</h3>";
+
+
+
+
+?>
+
+
+
+
+
+
+ <?php include "footer.php" ?>
