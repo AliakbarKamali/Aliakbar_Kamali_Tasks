be42c4cd655aeaa7e67e15b525fa950e12d0f972
AliakbarKamali <aliakbar.kamaliandani@student.hamk.fi>
2023-02-07 09:12:53 +0200

    creat

---
+++ layout/ex3/variable.php
@@ -0,0 +1,12 @@
+<?php include "header.php"?>
+
+<h3>Ex3 .In-class Task Variable & Operators 07.02.2023 (variable.php)</h3>
+<h2>
+    1.Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using <h3> tag: Hello …., You are welcome to my site.  (Form Tutorial: https://www.youtube.com/watch?v=nLPlDHrsyYA  
+</h2>
+<form>
+  First Name  <
+</form>
+
+
+ <?php include "footer.php" ?>
