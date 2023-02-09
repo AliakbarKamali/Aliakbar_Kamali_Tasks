04c824641df5834030d6c87b45cc4acf4f960466
AliakbarKamali <aliakbar.kamaliandani@student.hamk.fi>
2023-02-09 13:13:05 +0200

    average temperature

--- layout/array.php
+++ layout/array.php
@@ -140,6 +140,42 @@ foreach ($favorite_colors as $color => $hex) {
   echo "<br>";
 }
 ?>
+<h2>
+6. PHP script to calculate and display average temperature, five lowest and highest temperatures.
+</h2>
+Recorded temperatures : 78, 60, 62, 68, 71, 68, 73,
+ 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 
+ 74, 62, 62, 65, 64, 68, 73, 75, 79, 73
+<?php
+echo "<h4> Calculation average temperature: </h4>";
+$month_temp = "78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 81, 76, 73,
+68, 72, 73, 75, 65, 74, 63, 67, 65, 64, 68, 73, 75, 79, 73";
+// what is explode and what does the below code do? : 
+$temp_array = explode(',', $month_temp);
+$tot_temp = 0;
+// What is count?
+$temp_array_length = count($temp_array);
+foreach($temp_array as $temp)
+{
+ $tot_temp += $temp;
+}
+ $avg_high_temp = $tot_temp/$temp_array_length;
+ echo "Average Temperature is : ".$avg_high_temp."
+"; 
+// what does sort do?
+sort($temp_array);
+echo "<br> List of five lowest temperatures :";
+for ($i=0; $i< 5; $i++)
+{ 
+echo $temp_array[$i].", ";
+}
+echo "<br>List of five highest temperatures :";
+// explain the following loop
+for ($i=($temp_array_length-5); $i< ($temp_array_length); $i++)
+{
+echo $temp_array[$i].", ";
+}
+?>
 
  <?php include "footer.php" ?>
 
