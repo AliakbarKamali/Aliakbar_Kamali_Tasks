01d9028e12d106e846dc3d1f9998517835e127ac
AliakbarKamali <aliakbar.kamaliandani@student.hamk.fi>
2023-02-10 10:21:55 +0200

    creating connection

--- crud1/db.php
+++ crud1/db.php
@@ -1,4 +1,15 @@
 <?php
+$servername = "db-1";
+$username = "capp1";
+$password = "password";
+$dbname = "capp1";
+
+//creating connection
+$conn = new mysqli ($servername, $username, $password, $dbname);
+
+//check the connection
+if($conn->connect_error){
+    die("connection failed:" . $conn->connect_error);
 
 
 
