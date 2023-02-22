84ef20c4beab7487364a229f94268c38eae4979a
AliakbarKamali <aliakbar.kamaliandani@student.hamk.fi>
2023-02-22 13:33:48 +0200

    JavaScipt Evants

---
+++ layout/jsinclass.php
@@ -0,0 +1,16 @@
+<?php
+$title = "JavaScipt Evants and form validation";
+include "header.php";
+?>
+
+<h1> A simple button to display date</h1>
+<button onclick="this.innerHTML=Date()">Click to know date </button>
+
+<h2> Make a butoon and place holder as paragraph for the date</h2>
+<button onclick="getElementById('p1').innerHTML=Date()">Click to know date </button>
+<p id="p1"> </p>
+
+<h2> Change the background color</h2>
+<form> 
+<input type="color" name="background" onchange="changeColor('background', this.value);">
+</form>
