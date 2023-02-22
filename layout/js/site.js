233d01785599f8ebf8f26e9902cde00bd4903c6f
AliakbarKamali <aliakbar.kamaliandani@student.hamk.fi>
2023-02-22 15:34:51 +0200

    aa

--- layout/js/site.js
+++ layout/js/site.js
@@ -30,3 +30,38 @@ function changeColor(where,newColor){
     }
 }
 
+//crud
+function crud(){
+    let fname=(document.form1.fname.value).trim();
+    if (fname.length <5)
+    alert("First Name must have atleast 5 chsracter");
+    return false;
+}
+
+
+function fname(){
+    let fname=(document.form1.fname.value).trim();
+    if (fname.length <5)
+    alert("First Name must have atleast 5 chsracter");
+    return false;
+}
+
+//example of realtime input validation 
+function cname(){
+    let name=document.myform.name.value;  
+    // to check if name is empty of not  
+    if (name==null || name==""){  
+      alert("Name can't be blank");  
+      return false;  
+    }
+    // to check if name has atleast 5 characters
+    else if(name.length<5){  
+        alert("Name can't less than 5 characters");  
+        return false;  
+      }
+    // // to ensure name cannot be more than 5 characters
+    else if(name.length>15){  
+        alert("Name can't more than 15 characters");  
+        return false;  
+      }
+    }
